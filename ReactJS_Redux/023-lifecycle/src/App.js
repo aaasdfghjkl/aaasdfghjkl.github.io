3aa72b379c8b2ceb22695064d5f475fc61c744be
aaasdfghjkl <46653840+aaasdfghjkl@users.noreply.github.com>
2019-01-17 01:06:28 +0700

    b26 update state

--- ReactJS_Redux/023-lifecycle/src/App.js
+++ ReactJS_Redux/023-lifecycle/src/App.js
@@ -1,30 +1,48 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sstate: 'khoi tao'
-    }
+      sstate: "khoi tao"
+    };
   }
-  
+
   componentWillMount() {
-    console.log('component will mount');
-    
+    console.log("component will mount");
   }
   componentDidMount() {
-    console.log('component did mount');
-    
+    console.log("component did mount");
   }
-  
-  
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate");
+    // return false;
+    return true;
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.log("componentWillUpdate");
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate");
+  }
+
+  updateState = () => {
+    this.setState({ sstate: "trang thai duoc update" });
+  };
+
   render() {
-    console.log('render');
-    
+    console.log("render, trạng thái:  => " + this.state.sstate);
+
     return (
       <div className="App">
-       
+        <button
+          onClick={() => {
+            this.updateState();
+          }}
+        >
+          click update state
+        </button>
       </div>
     );
   }
